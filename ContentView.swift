dd27f4be0af5ffc201c41896323c406558cdcb7f
James Buda <jamesmbuda@gmail.com>
2024-09-08 21:59:47 -0700

    adding default swift code

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(Color(red: 0, green: 0, blue: 255))
+            Text("BLUE")
+        }
+    }
+}
